506bf843688f0e389a308b5790466ae607e4e5f6
Alok Kumar <alok.kumar@ovyo.com>
2025-04-16 21:30:29 +0530

    Initial Commit

---
+++ DemoLogin/DemoLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoLoginApp.swift
+//  DemoLogin
+//
+//  Created by Alok Kumar on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
